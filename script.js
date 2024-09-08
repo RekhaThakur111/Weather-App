f020c93e524cf9093c90a213e3313fc057d91cd5
Rekha Thakur <rekhathakur85347@gmail.com>
2024-09-08 16:54:53 +0530

    fbfh

--- script.js
+++ script.js
@@ -37,13 +37,13 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "./cloud.png";
+            weather_img.src = "assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "./clear.png";
+            weather_img.src = "./assets/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "./rain.png";
+            weather_img.src = "/assets/rain.png";
             break;
         case 'Mist':
             weather_img.src = "./mist.png";
